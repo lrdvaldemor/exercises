0176cb116d988570ae8b6c58173df632383ae822
HP <lrdvldmr@gmail.com>
2021-07-22 20:12:50 +0300

    exs 1

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log('Hello')
+
+document.getElementById('out').innerHTML = 'Hello'
+document.querySelector('h2.header').innerHTML = '22';
+console.log('Владимир1')
+console.log(1)
+console.log('\'Добро \'+\'пожаловать \'+\' на курс\'')
+// alert(2019)
+// alert(2019 - 200)
+
+// меняем содержимое див
+document.getElementById('one').innerHTML = 'Hello World'
+
+// меняем содержимое див
+document.getElementById('one').innerHTML = 12 * 12
+
+//див с классом ван
+document.querySelector('.one').innerHTML = 'Привет'
+
+
+//див с классом ту
+document.querySelector('span').innerHTML = 'Привет'
+
+
+//див с классом три
+document.querySelector('.three').innerHTML = '<h3>третий параграф</h3>'
+
